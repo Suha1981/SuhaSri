78370bf98f24ea4bda648246922d1915c1f9f7ea
Suhasri <suhanishu29@gmail.com>
2022-05-25 07:57:34 +1000

    profile tests

--- Sri/Program.cs
+++ Sri/Program.cs
@@ -41,6 +41,50 @@ namespace Sri
             {
                 Console.WriteLine("Login failed, test failed,");
             }
+
+            // create Time and Material Record
+
+            // go to TM page
+
+            IWebElement AdministrationDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li(5)/a"));
+            AdministrationDropdown.Click();
+
+            IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]"));
+            tmOption.Click();
+
+            // Click on create New Button
+            IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id='container']/p/a"));
+            createNewButton.Click();
+
+            // select material from typeccode dropdown
+            IWebElement typecodedropdown = driver.FindElement(By.XPath("//*[@id='TimematerialEditForm']/div/[1]/div//span/span[2]/span"));
+            typecodedropdown.Click();
+
+            IWebElement materialoption = driver.FindElement(By.XPath("//*[@id='TypeCode'_Listbox']/Li[1]"));
+            materialoption.Click();
+
+            // Identify the code textbox and put a code
+            IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+            codeTextbox.SendKeys("April2022");
+
+            // Identify the description textbox and put a description
+            IWebElement descriptiontextbox = driver.FindElement(By.Id("Description"));
+            descriptiontextbox.SendKeys("April2022");
+
+            // Idetify the Price per unit text box and out a price
+            IWebElement Pricetextbox = driver.FindElement(By.Id("Price"));
+            Pricetextbox.SendKeys("April2022");
+
+            // Click on Save Button
+            IWebElement Save = driver.FindElement(By.Id("SaveButton"));
+            Save.Click();
+
+
+
+
+
+
         }
+
     }
 }
